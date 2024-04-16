bced022e38e8d311dc5f6cdb92285726c7acec7c
quentinalbrecht <albrecht.quentin.qa@icloud.com>
2024-04-16 18:04:48 +0200

    added JavaDoc

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,8 @@
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+//@param patientID takes a certain patient, @param outputStrategy determines what data is needed
+//interaface produces the needed patient data
 public interface PatientDataGenerator {
     void generate(int patientId, OutputStrategy outputStrategy);
 }
