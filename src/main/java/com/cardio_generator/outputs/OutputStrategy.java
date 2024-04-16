6d95798a0dffe73b075c3f96904d7a5807d92574
Bram Temming <Bram.Temming@construsoft.com>
2024-04-16 17:47:06 +0200

    Add commnets to OutputStrategy

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,16 @@
-package com.cardio_generator.outputs;
-
+/**
+ * The OutputStrategy interface defines a contract for classes that provide output strategies for health data simulation.
+ * Implementations of this interface handle the output of simulated health data for a given patient.
+ */
 public interface OutputStrategy {
+    
+    /**
+     * Outputs simulated health data for a patient.
+     * 
+     * @param patientId The ID of the patient for whom the data is being simulated.
+     * @param timestamp The timestamp of the simulated data.
+     * @param label     The label or type of the simulated data.
+     * @param data      The simulated data to be output.
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
